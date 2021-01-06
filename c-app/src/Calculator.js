d23ec7c9edaab228d16ef576b132e1300f3f3044
Ceddyp19 <ceddyp117@gmail.com>
2021-01-05 22:14:16 -0600

    add children to Calculator

--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -1,10 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import FormulaScreen from './FormulaScreen';
+import OutputScreen from './OutputScreen';
 
 class Calculator extends Component {
-    state = {  }
-    render() { 
-        return ( <div id="calculator" style={{backgroundColor: 'blue'}}>My Calculator</div> );
+    state = {}
+    render() {
+        return (
+            <div id="calculator" style={{ backgroundColor: 'blue' }}>
+                My Calculator
+                <FormulaScreen />
+                <OutputScreen />
+            </div>
+        );
     }
 }
- 
+
 export default Calculator;
