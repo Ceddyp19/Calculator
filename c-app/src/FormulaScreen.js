5d48e143109834fe04e69a3586c5c228b5ab8c41
Ceddyp19 <ceddyp117@gmail.com>
2021-01-05 22:10:10 -0600

    Created formulascreen and output screen components

---
+++ c-app/src/FormulaScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class FormulaScreen extends Component {
+    state = {  }
+    render() { 
+        return ( <div>FormulaScreen</div> );
+    }
+}
+ 
+export default FormulaScreen;
---
+++ c-app/src/OutputScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class OutputScreen extends Component {
+    state = {}
+    render() {
+        return (<div>Output</div>);
+    }
+}
+
+export default OutputScreen;
