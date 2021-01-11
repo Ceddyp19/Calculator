93122e01f4c77fab95300907ca10cd1e0c0bf910
Ceddyp19 <ceddyp117@gmail.com>
2021-01-10 20:06:27 -0600

    added child comps

--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -9,6 +9,16 @@ class Calculator extends Component {
             <div id="calculator" style={{ backgroundColor: 'blue' }}>
                 <FormulaScreen />
                 <OutputScreen />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
+                <Calculator />
             </div>
         );
     }
