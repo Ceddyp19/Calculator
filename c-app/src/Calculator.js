1496c36faf2babcd02d7024a4f2518c08537323d
Ceddyp19 <ceddyp117@gmail.com>
2021-01-16 23:51:15 -0600

    more

--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -25,4 +25,5 @@ class Calculator extends Component {
 }
 //Calculator is for the something, I don't know what i'm saying obviously 
 //just a little extra code for absolutely nothing but to annoy myself
+//more foolishness
 export default Calculator;
