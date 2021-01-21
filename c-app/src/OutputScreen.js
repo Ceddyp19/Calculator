a7952ab82138851d9adbbd88e04b5f4e775dff83
Ceddyp19 <ceddyp117@gmail.com>
2021-01-21 00:12:10 -0600

    gave output screen an id of display

--- c-app/src/OutputScreen.js
+++ c-app/src/OutputScreen.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class OutputScreen extends Component {
     state = {}
     render() {
-        return (<div className="outp-scrn" style={{ backgroundColor: 'green' }}>Output</div>);
+        return (<div id="display" style={{ backgroundColor: 'green' }}>Output</div>);
     }
 }
 
