26c1ac1184f72ef3bffe2c2799f53500bb6efb77
Ceddyp19 <ceddyp117@gmail.com>
2021-01-22 23:19:30 -0600

    created a clearDisplay function in Calculator

--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -3,29 +3,34 @@ import FormulaScreen from './FormulaScreen';
 import OutputScreen from './OutputScreen';
 import Button from './Button'
 class Calculator extends Component {
-    state = {displayText: 0}
+    state = { displayText: 0 }
+
+    clearDisplay = () => {
+        this.setState({ displayText: 0 })
+    }
+
     render() {
         return (
             <div id="calculator" style={{ backgroundColor: 'blue' }}>
-                <FormulaScreen displayText={this.state.displayText}/>
+                <FormulaScreen displayText={this.state.displayText} />
                 <OutputScreen />
-                <Button class='number' id='equals' text='='/>
-                <Button class='number' id='zero' text='0'/>
-                <Button class='number' id='one' text='1'/>
-                <Button class='number' id='two' text='2'/>
-                <Button class='number' id='three' text='3'/>
-                <Button class='number' id='four' text='4'/>
-                <Button class='number' id='five' text='5'/>
-                <Button class='number' id='six' text='6'/>
-                <Button class='number' id='seven' text='7'/>
-                <Button class='number' id='eight' text='8'/>
-                <Button class='number' id='nine' text='9'/>
-                <Button class='arithmetic' id='add' text='+'/>
-                <Button class='arithmetic' id='subtract' text='-'/>
-                <Button class='arithmetic' id='multiply' text='*'/>
-                <Button class='arithmetic' id='divide' text='/'/>
-                <Button id='decimal' text='.'/>
-                <Button id='clear' text='clear'/>
+                <Button class='number' id='equals' text='=' />
+                <Button class='number' id='zero' text='0' />
+                <Button class='number' id='one' text='1' />
+                <Button class='number' id='two' text='2' />
+                <Button class='number' id='three' text='3' />
+                <Button class='number' id='four' text='4' />
+                <Button class='number' id='five' text='5' />
+                <Button class='number' id='six' text='6' />
+                <Button class='number' id='seven' text='7' />
+                <Button class='number' id='eight' text='8' />
+                <Button class='number' id='nine' text='9' />
+                <Button class='arithmetic' id='add' text='+' />
+                <Button class='arithmetic' id='subtract' text='-' />
+                <Button class='arithmetic' id='multiply' text='*' />
+                <Button class='arithmetic' id='divide' text='/' />
+                <Button id='decimal' text='.' />
+                <Button id='clear' text='clear' />
 
             </div>
         );
