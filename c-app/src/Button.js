885833eb915f826db85b62177029af65bcdfbc93
Ceddyp19 <ceddyp117@gmail.com>
2021-01-29 23:01:47 -0600

    added comment

--- c-app/src/Button.js
+++ c-app/src/Button.js
@@ -1,6 +1,6 @@
 
 function Button(props) {
-
+//whenever a better is clicked, action prop is activated and we pass the buttons' arithmetic value as parameter  
         return (
         <button className={props.class} id={props.id} onClick={() => props.action(props.num)}>{props.text}</button> 
         );
