efebe19210643b49c4e2e1e82d101af59dff3876
Ceddyp19 <ceddyp117@gmail.com>
2021-01-30 23:27:50 -0600

    no

--- c-app/src/Button.js
+++ c-app/src/Button.js
@@ -2,7 +2,7 @@
 function Button(props) {
 //whenever a better is clicked, action prop is activated and we pass the buttons' arithmetic value as parameter  
         return (
-        <button className={props.class} id={props.id} onClick={() => props.action(props.num)}>{props.text}</button> 
+        <button className={props.class} id={props.id} onClick={() => props.action(props.text)}>{props.text}</button> 
         );
     
 }
--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -4,20 +4,20 @@ import OutputScreen from './OutputScreen';
 import Button from './Button'
 class Calculator extends Component {
     state = {
-        formulaScreenText: 0,
+        formulaScreenText: '',
         outputScreenText: '0'
     }
 
     clearDisplay = () => {
         this.setState({
-            formulaScreenText: 0,
+            formulaScreenText: '',
             outputScreenText: '0'
         })
 
     }
 
     inputNum = (num) => {
-        this.setState({formulaScreenText: num})
+        this.setState({formulaScreenText: this.state.formulaScreenText + num})
     }
 
     render() {
@@ -25,17 +25,17 @@ class Calculator extends Component {
             <div id="calculator" style={{ backgroundColor: 'blue' }}>
                 <FormulaScreen displayText={this.state.formulaScreenText} />
                 <OutputScreen outputText={this.state.outputScreenText}/>
-                <Button class='number' id='equals' text='=' num={1}/>
-                <Button class='number' id='zero' text='0' num={1}/>
+                <Button class='number' id='equals' text='=' />
+                <Button class='number' id='zero' text='0' num={0} action={this.inputNum}/>
                 <Button class='number' id='one' text='1' num={1} action={this.inputNum}/>
-                <Button class='number' id='two' text='2' num={2}/>
-                <Button class='number' id='three' text='3' />
-                <Button class='number' id='four' text='4' />
-                <Button class='number' id='five' text='5' />
-                <Button class='number' id='six' text='6' />
-                <Button class='number' id='seven' text='7' />
-                <Button class='number' id='eight' text='8' />
-                <Button class='number' id='nine' text='9' />
+                <Button class='number' id='two' text='2' num={2} action={this.inputNum}/>
+                <Button class='number' id='three' text='3' num={3} action={this.inputNum}/>
+                <Button class='number' id='four' text='4' num={4} action={this.inputNum}/>
+                <Button class='number' id='five' text='5' num={5} action={this.inputNum}/>
+                <Button class='number' id='six' text='6' num={6} action={this.inputNum}/>
+                <Button class='number' id='seven' text='7' num={7} action={this.inputNum}/>
+                <Button class='number' id='eight' text='8' num={8} action={this.inputNum}/>
+                <Button class='number' id='nine' text='9' num={9} action={this.inputNum}/>
                 <Button class='arithmetic' id='add' text='+' />
                 <Button class='arithmetic' id='subtract' text='-' />
                 <Button class='arithmetic' id='multiply' text='*' />
@@ -49,3 +49,5 @@ class Calculator extends Component {
 }
 
 export default Calculator;
+
+//will i ever finish this?
