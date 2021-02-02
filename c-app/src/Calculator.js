245b01b46f7c31bd898827589edaaf3f839cf2ec
Ceddyp19 <ceddyp117@gmail.com>
2021-02-01 19:59:46 -0600

    updated the props of the button components

--- c-app/src/Calculator.js
+++ c-app/src/Calculator.js
@@ -12,6 +12,7 @@ class Calculator extends Component {
         switch (input) {
             case 'clear':
                 console.log('it should clear')
+                this.setState({formulaScreenText: '0', outputScreenText: '0'})
                 break;
             case '+':
                 console.log('it should add')
@@ -27,6 +28,8 @@ class Calculator extends Component {
                 break;
             default:
                 console.log('this is the default')
+                //use a terary statement to remove leading zero before adding digit to the formula screen text
+                //for ex. without terary, if the 1st num a user typed is 2, '02' would have been displayed on screen 
                 this.state.formulaScreenText === '0' ? this.setState({ formulaScreenText: input }) : this.setState({ formulaScreenText: this.state.formulaScreenText + input });
                 break;
         }
@@ -43,20 +46,20 @@ class Calculator extends Component {
                 <FormulaScreen displayText={this.state.formulaScreenText} />
                 <OutputScreen outputText={this.state.outputScreenText} />
                 <Button class='number' id='equals' text='=' />
-                <Button class='number' id='zero' text='0' num={0} action={this.inputNum} />
-                <Button class='number' id='one' text='1' num={1} action={this.inputNum} />
-                <Button class='number' id='two' text='2' num={2} action={this.inputNum} />
-                <Button class='number' id='three' text='3' num={3} action={this.inputNum} />
-                <Button class='number' id='four' text='4' num={4} action={this.inputNum} />
-                <Button class='number' id='five' text='5' num={5} action={this.inputNum} />
-                <Button class='number' id='six' text='6' num={6} action={this.inputNum} />
-                <Button class='number' id='seven' text='7' num={7} action={this.inputNum} />
+                <Button class='number' id='zero' text='0' handleInput={this.handleInput} />
+                <Button class='number' id='one' text='1' handleInput={this.handleInput} />
+                <Button class='number' id='two' text='2' handleInput={this.handleInput} />
+                <Button class='number' id='three' text='3' handleInput={this.handleInput} />
+                <Button class='number' id='four' text='4' handleInput={this.handleInput} />
+                <Button class='number' id='five' text='5' handleInput={this.handleInput} />
+                <Button class='number' id='six' text='6' handleInput={this.handleInput} />
+                <Button class='number' id='seven' text='7' handleInput={this.handleInput} />
                 <Button class='number' id='eight' text='8' handleInput={this.handleInput} />
                 <Button class='number' id='nine' text='9' handleInput={this.handleInput} />
-                <Button class='arithmetic' id='add' text='+' handleInput={this.handleInput}/>
-                <Button class='arithmetic' id='subtract' text='-' handleInput={this.handleInput}/>
-                <Button class='arithmetic' id='multiply' text='*' handleInput={this.handleInput}/>
-                <Button class='arithmetic' id='divide' text='/' handleInput={this.handleInput}/>
+                <Button class='arithmetic' id='add' text='+' handleInput={this.handleInput} />
+                <Button class='arithmetic' id='subtract' text='-' handleInput={this.handleInput} />
+                <Button class='arithmetic' id='multiply' text='*' handleInput={this.handleInput} />
+                <Button class='arithmetic' id='divide' text='/' handleInput={this.handleInput} />
                 <Button id='decimal' text='.' />
                 <Button id='clear' text='clear' handleInput={this.handleInput} />
 
